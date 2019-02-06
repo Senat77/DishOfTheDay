2d40e3edf83686008cc4fc296bb9d5055c58c4d6
Sergii Senatorov <sergii.senatorov@gmail.com>
2019-02-06 19:18:00 +0200

    RestaurantServiceTest

---
+++ src/test/java/rest/DishOfTheDay/service/RestaurantServiceTest.java
@@ -0,0 +1,28 @@
+package rest.DishOfTheDay.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RestaurantServiceTest {
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void delete() {
+    }
+}
