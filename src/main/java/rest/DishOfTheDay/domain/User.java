310f00a1c98684b2acf26dea3b75918dab0393ed
Sergii Senatorov <sergii.senatorov@gmail.com>
2019-02-07 20:12:09 +0200

    User

--- src/main/java/rest/DishOfTheDay/domain/User.java
+++ src/main/java/rest/DishOfTheDay/domain/User.java
@@ -1,15 +1,17 @@
 package rest.DishOfTheDay.domain;
 
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+
+import static org.springframework.security.config.Elements.PASSWORD_ENCODER;
 
 @Entity
 @Table(name = "users")
@@ -33,22 +35,38 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private boolean enabled = true;
 
+    /*
     @NotNull
     @Column(nullable = false)
     private Role role = ROLE_USER;
+    */
 
     @NotNull
     @Column(nullable = false)
     private LocalDateTime registered;
 
+    /*
     public User(@NotNull String email, @NotNull String name, @NotNull String password, @NotNull boolean enabled, Role role, @NotNull LocalDateTime registered) {
         this.email = email;
         this.name = name;
-        this.password = password;
+        setPassword(password);
         this.enabled = enabled;
-        this.role = role;
+        //this.role = role;
         this.registered = registered;
     }
+    */
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    /*
+    @PrePersist
+    private void encodePassword() {
+        password = (password != null) ? PASSWORD_ENCODER.encode(password) : null;
+    }
+
 
     public enum Role implements GrantedAuthority {
 
@@ -57,4 +75,5 @@ public class User extends BaseEntity {
             return null;
         }
     }
+    */
 }
