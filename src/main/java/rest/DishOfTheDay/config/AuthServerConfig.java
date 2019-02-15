4c267232662dda01c39329fd1311ccac0b928bb7
Sergii Senatorov <sergii.senatorov@gmail.com>
2019-02-15 16:31:45 +0200

    User

--- src/main/java/rest/DishOfTheDay/config/AuthServerConfig.java
+++ src/main/java/rest/DishOfTheDay/config/AuthServerConfig.java
@@ -1,9 +1,15 @@
 package rest.DishOfTheDay.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@Configuration
+@EnableAuthorizationServer
+@EnableResourceServer
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
     private final AuthenticationManager authenticationManager;
@@ -15,8 +21,8 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     @Override
     public void configure(ClientDetailsServiceConfigurer clientDetailsService) throws Exception {
         clientDetailsService.inMemory()
-                .withClient("client")
-                .secret("{noop}")
+                .withClient("DOTDclient")
+                .secret("{noop}dotdclient")
                 .scopes("*")
                 .authorizedGrantTypes("password", "refresh_token")
                 .accessTokenValiditySeconds(60 * 2) // 2 min
--- src/main/java/rest/DishOfTheDay/config/SecurityConfig.java
+++ src/main/java/rest/DishOfTheDay/config/SecurityConfig.java
@@ -1,16 +1,19 @@
 package rest.DishOfTheDay.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 // https://github.com/Cepr0/dependent-security/blob/master/src/main/java/io/github/cepr0/dependent_security/security/SecurityConfig.java
 // https://stackoverflow.com/questions/54149737/how-to-generate-a-jwt-access-token-with-some-custom-claims-in-it/54245750#54245750
@@ -28,13 +31,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserService).passwordEncoder(PASSWORD_ENCODER);;
+        auth.userDetailsService(customUserService).passwordEncoder(PASSWORD_ENCODER);
     }
 
     @Bean
     @Override
     public AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/api/admin/users").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(STATELESS)
+                .and()
+                .csrf().disable()
+                .formLogin().disable()
+                .logout().disable();
     }
 }
 
