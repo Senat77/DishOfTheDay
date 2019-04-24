ad7988c19b344b2aafb482df3bbaa200680c565c
Sergii Senatorov <sergii.senatorov@gmail.com>
2019-04-24 14:23:36 +0300

    Test Data Population(https://github.com/database-rider/database-rider)

--- src/test/java/rest/DishOfTheDay/repository/MenuRepositoryTest.java
+++ src/test/java/rest/DishOfTheDay/repository/MenuRepositoryTest.java
@@ -1,20 +1,17 @@
 package rest.DishOfTheDay.repository;
 
+import com.github.database.rider.core.api.dataset.DataSet;
 import org.junit.jupiter.api.*;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Profile;
-import rest.DishOfTheDay.TestDataPopulation;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import java.time.LocalDate;
 
 @Profile("test")
 @DataJpaTest
-public class MenuRepositoryTest extends TestDataPopulation {
+public class MenuRepositoryTest {
 
     @Autowired
     private MenuRepository menuRepository;
@@ -22,13 +19,8 @@ public class MenuRepositoryTest extends TestDataPopulation {
     @Autowired
     private RestaurantRepository restaurantRepository;
 
-    @BeforeEach
-    public void setUp() {
-        restaurantRepository.saveAll(restaurantPopulate());
-        menuRepository.saveAll(menuPopulate());
-    }
-
     @Test
+    @DataSet({"restaurants.yml"})
     public void getLastMenuByRestaurant() {
         assertThat(menuRepository.getLastMenuByRestaurant(restaurant1).getDate()).isEqualTo(LocalDate.now());
     }
