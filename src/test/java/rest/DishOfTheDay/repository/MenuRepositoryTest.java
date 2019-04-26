e17090abf27a05c8f85d0ec27a93160d1fa95020
Sergii Senatorov <sergii.senatorov@gmail.com>
2019-04-26 13:39:34 +0300

    MenuRepositoryTest (@Sql-annotation data population)

--- src/test/java/rest/DishOfTheDay/repository/MenuRepositoryTest.java
+++ src/test/java/rest/DishOfTheDay/repository/MenuRepositoryTest.java
@@ -1,25 +1,34 @@
 package rest.DishOfTheDay.repository;
 
 import com.github.database.rider.core.DBUnitRule;
+import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.dataset.DataSet;
-import com.github.database.rider.core.util.EntityManagerProvider;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import rest.DishOfTheDay.DishOfTheDayApplication;
 import rest.DishOfTheDay.domain.Restaurant;
+
+import static junit.framework.TestCase.assertNull;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDate;
 
-//@Profile("test")
+@Profile("test")
+@RunWith(SpringRunner.class)
 @DataJpaTest
 public class MenuRepositoryTest {
-
-    @Rule
-    public EntityManagerProvider emProvider = EntityManagerProvider.instance("riderDB");
-
-    @Rule
-    public DBUnitRule dbUnitRule = DBUnitRule.instance(emProvider.connection());
 
     @Autowired
     private MenuRepository menuRepository;
@@ -28,16 +37,16 @@ public class MenuRepositoryTest {
     private RestaurantRepository restaurantRepository;
 
     @Test
-    @DataSet(value = {"restaurants.yml"})
+    @Sql(scripts = {"/datasets/test-restaurants-data.sql"})
     public void getLastMenuByRestaurant() {
         Restaurant restaurant = restaurantRepository.findByName("Restaurant1");
-        System.out.println(restaurant.getAddress());
         assertThat(menuRepository.getLastMenuByRestaurant(restaurant).getDate().isEqual(LocalDate.now()));
     }
 
-    /*@Test
-    //public void getLastMenuByRestaurant_NotFound() {
-        assertNull(menuRepository.getLastMenuByRestaurant(restaurant4));
+    @Test
+    @Sql(scripts = {"/datasets/test-restaurants-data.sql"})
+    public void getLastMenuByRestaurant_NotFound() {
+        Restaurant restaurant = restaurantRepository.findByName("Restaurant4");
+        assertNull(menuRepository.getLastMenuByRestaurant(restaurant));
     }
-     */
 }
---
+++ src/test/resources/datasets/test-restaurants-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO RESTAURANTS (ID, VERSION, ADDRESS, EMAIL, NAME) VALUES (101, 0, 'Address1', 'restaurant1@site.com', 'Restaurant1');
+INSERT INTO RESTAURANTS (ID, VERSION, ADDRESS, EMAIL, NAME) VALUES (102, 0, 'Address2', null, 'Restaurant2');
+INSERT INTO RESTAURANTS (ID, VERSION, ADDRESS, EMAIL, NAME) VALUES (103, 0, 'Address3', 'restaurant3@site.com', 'Restaurant3');
+INSERT INTO RESTAURANTS (ID, VERSION, ADDRESS, EMAIL, NAME) VALUES (104, 0, 'Address4', 'restaurant4@site.com', 'Restaurant4');
