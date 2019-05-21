ef7401d91b9a93736176dc7c4ea24f9a9c3e52af
Sergii Senatorov <sergii.senatorov@gmail.com>
2019-05-21 15:32:18 +0300

    UserServiceTest (completed)

--- src/test/java/rest/DishOfTheDay/service/UserServiceTest.java
+++ src/test/java/rest/DishOfTheDay/service/UserServiceTest.java
@@ -1,9 +1,12 @@
 package rest.DishOfTheDay.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
@@ -21,16 +24,22 @@ import static org.junit.Assert.assertEquals;
 @Profile("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@SqlGroup({
-        @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
-                scripts = "/TestData/test-users-data.sql") })
+@SqlGroup({@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+           scripts = "/TestData/test-users-data.sql")})
 public class UserServiceTest {
 
     @Autowired
     private UserService userService;
 
+    @Autowired
+    CacheManager cacheManager;
+
+    @Before
+    public void beforeTest() {
+        cacheManager.getCache("users").clear();
+    }
+
     @Test
-    //@Sql(scripts = {"/TestData/test-users-data.sql"})
     public void getAll() {
         List<UserRespDTO> users = userService.getAll();
         assertThat(users).hasSize(6);
@@ -38,30 +47,26 @@ public class UserServiceTest {
     }
 
     @Test
-    //@Sql(scripts = {"/TestData/test-users-data.sql"})
-    public void get() throws EntityNotFoundException {
+    public void get() {
         assertThat(userService.get(3).getName()).isEqualTo("user3");
     }
 
     @Test
-    //@Sql(scripts = {"/TestData/test-users-data.sql"})
-    public void create() throws EntityNotFoundException {
+    public void create() {
         UserReqDTO userDTO = new UserReqDTO("newuser", "newuser@site.com", "newpassword");
         int id = userService.create(userDTO).getId();
         assertEquals(userService.get(id).getEmail(), "newuser@site.com");
     }
 
     @Test
-    //@Sql(scripts = {"/TestData/test-users-data.sql"})
-    public void update() throws EntityNotFoundException {
+    public void update() {
         UserReqDTO userDTO = new UserReqDTO("admin", "newmail@site.com", "newpassword");
         UserRespDTO updateUser = userService.update(0, userDTO);
         assertEquals(userService.get(updateUser.getId()).getEmail(), "newmail@site.com");
     }
 
     @Test
-    //@Sql(scripts = {"/TestData/test-users-data.sql"})
-    public void delete() throws EntityNotFoundException {
+    public void delete() {
         UserRespDTO userDTO = userService.get(0);
         userService.delete(0);
         assertThat(userService.getAll()).doesNotContain(userDTO);
