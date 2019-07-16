648de2a56e41717a4f77a10fdd7c9a11ac812398
Sergii Senatorov <sergii.senatorov@gmail.com>
2019-07-16 15:02:30 +0300

    PollServiceTest (not completed)

--- src/test/java/rest/DishOfTheDay/service/PollServiceTest.java
+++ src/test/java/rest/DishOfTheDay/service/PollServiceTest.java
@@ -2,11 +2,18 @@ package rest.DishOfTheDay.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
+import rest.DishOfTheDay.domain.Poll;
+import rest.DishOfTheDay.domain.dto.PollReqDTO;
+import rest.DishOfTheDay.domain.dto.PollRespDTO;
+
+import java.time.LocalDate;
+import java.util.Set;
 
 @Profile("test")
 @RunWith(SpringRunner.class)
@@ -18,8 +25,15 @@ import org.springframework.test.context.junit4.SpringRunner;
                 "/TestData/test-polls-data.sql"})})
 public class PollServiceTest {
 
+    @Autowired
+    PollService service;
+
     @Test
     public void create() {
+        PollReqDTO pollReqDTO = new PollReqDTO();
+        pollReqDTO.setId(LocalDate.now().plusDays(1));
+        pollReqDTO.setMenu_id(Set.of(204, 205));
+        PollRespDTO pollRespDTO = service.create(pollReqDTO);
     }
 
     @Test
