2d40cd980967ffba451495a4fab2b94a95a751a6
Sergii Senatorov <sergii.senatorov@gmail.com>
2019-10-23 15:42:20 +0300

    UserRestControllerTest

--- src/test/java/rest/DishOfTheDay/controller/UserRestControllerTest.java
+++ src/test/java/rest/DishOfTheDay/controller/UserRestControllerTest.java
@@ -2,15 +2,19 @@ package rest.DishOfTheDay.controller;
 
 import org.junit.Test;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import rest.DishOfTheDay.domain.dto.RestaurantRespDTO;
 import rest.DishOfTheDay.domain.dto.UserReqDTO;
 import rest.DishOfTheDay.domain.dto.UserRespDTO;
-
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SqlGroup({@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
@@ -34,22 +38,55 @@ public class UserRestControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    public void getAll() {
+    @WithMockUser(roles = "ADMIN")
+    public void getAll() throws Exception {
+        mvc.perform(get(UserRestController.REST_URL)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(6)))
+                .andExpect(jsonPath("$[0].name", is("admin")))
+                .andExpect(jsonPath("$[5].name", is("user5")));
     }
 
     @Test
-    public void delete() {
+    @WithMockUser(roles = "ADMIN")
+    public void delete() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.delete(UserRestController.REST_URL + "/3/delete")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
     @Test
-    public void getMe() {
+    @WithUserDetails("user3")
+    public void getMe() throws Exception {
+        mvc.perform(get(UserRestController.REST_URL + "/profile")
+                .secure(true)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is("user3")))
+                .andExpect(jsonPath("$.email", is("user3@site.com")));
     }
 
     @Test
-    public void editMe() {
+    @WithUserDetails("user3")
+    public void editMe() throws Exception {
+        UserReqDTO userReqDTO = new UserReqDTO("NewUser3", "user3@newmail.ua", "");
+        String inputJson = super.mapToJson(userReqDTO);
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.patch(UserRestController.REST_URL + "/profile")
+                .contentType(MediaType.APPLICATION_JSON_VALUE).content(inputJson))
+                .andExpect(status().isOk())
+                .andReturn();
+        String content = mvcResult.getResponse().getContentAsString();
+        UserRespDTO userRespDTO = super.mapFromJson(content, UserRespDTO.class);
+        assertEquals(userRespDTO.getName(), "NewUser3");
+        assertEquals(userRespDTO.getEmail(), "user3@newmail.ua");
     }
 
     @Test
-    public void deleteMe() {
+    @WithUserDetails("user3")
+    public void deleteMe() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.delete(UserRestController.REST_URL + "/profile")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
